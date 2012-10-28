b1b956b422f45a337ace319a11424495cd050584
Andras Wacha <awacha@gmail.com>
2012-10-28 09:24:08 +0100

    Re-based SASException to BaseException

--- sastool/misc/__init__.py
+++ sastool/misc/__init__.py
@@ -21,7 +21,7 @@ from arithmetic import *
 from rc import sastoolrc
 HC = 12398.419 #Planck's constant times speed of light, in eV*Angstrom units
 
-class SASException(Exception):
+class SASException(BaseException):
     "General exception class for the package `sastool`"
     pass
 
